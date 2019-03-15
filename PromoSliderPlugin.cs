15da459275642f177cd85cc09685493290a1c714
MacHeathz <MacHeathz@users.noreply.github.com>
2019-03-16 00:19:40 +0100

    Initial Commit

---
+++ PromoSliderPlugin.cs
@@ -0,0 +1,37 @@
+﻿using Nop.Core.Plugins;
+using Nop.Services.Cms;
+using System;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Widgets.PromoSlider
+{
+    public class PromoSliderPlugin : IWidgetPlugin
+    {
+        PluginDescriptor IPlugin.PluginDescriptor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        string IPlugin.GetConfigurationPageUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        string IWidgetPlugin.GetWidgetViewComponentName(string widgetZone)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<string> IWidgetPlugin.GetWidgetZones()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPlugin.Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPlugin.Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
