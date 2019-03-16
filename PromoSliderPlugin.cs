8d982d3b6ec6fdcdaf14b80acad0d137555f4b75
MacHeathz <MacHeathz@users.noreply.github.com>
2019-03-16 23:01:41 +0100

    Include plugin menu on Admin dashboard

--- PromoSliderPlugin.cs
+++ PromoSliderPlugin.cs
@@ -2,11 +2,13 @@
 using Nop.Core.Plugins;
 using Nop.Plugin.Widgets.PromoSlider.Data;
 using Nop.Services.Cms;
+using Nop.Web.Framework.Menu;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Nop.Plugin.Widgets.PromoSlider
 {
-    public class PromoSliderPlugin : BasePlugin, IWidgetPlugin
+    public class PromoSliderPlugin : BasePlugin, IWidgetPlugin, IAdminMenuPlugin
     {
         private readonly PromoSliderObjectContext _context;
         private readonly IWebHelper _webHelper;
@@ -44,6 +46,44 @@ namespace Nop.Plugin.Widgets.PromoSlider
         {
             _context.Uninstall();
             base.Uninstall();
+        }
+
+        public void ManageSiteMap(SiteMapNode rootNode)
+        {
+            var createUpdateNode = new SiteMapNode()
+            {
+                Visible = true,
+                Title = "New Slider",
+                SystemName = "New Slider",
+                IconClass= "fa-genderless",
+                Url = "/PromoSlider/CreateUpdatePromoSlider"
+            };
+
+            var manageSliders = new SiteMapNode()
+            {
+                Visible = true,
+                Title = "Manage Sliders",
+                SystemName = "Manage Sliders",
+                IconClass= "fa-genderless",
+                Url = "/PromoSlider/ManagePromoSliders"
+            };
+
+            var parentNode = new SiteMapNode()
+            {
+                Visible = true,
+                Title = "Promo Slider",
+                SystemName = "Promo Slider",
+                IconClass= "fa-dot-circle-o"
+            };
+
+            parentNode.ChildNodes.Add(createUpdateNode);
+            parentNode.ChildNodes.Add(manageSliders);
+
+            var pluginNode = rootNode.ChildNodes.FirstOrDefault(x => x.SystemName == "Third party plugins");
+            if (pluginNode != null)
+                pluginNode.ChildNodes.Add(parentNode);
+            else
+                rootNode.ChildNodes.Add(parentNode);
         }
     }
 }
